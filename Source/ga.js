6a12ddc41b4cbb8fc10ebe9f7dc2339332a44f19
Shubham Kumar <shubhamkrai123@gmail.com>
2019-03-14 21:17:45 +0530

    Update game

---
+++ Source/ga.js
@@ -0,0 +1,76 @@
+// Start the game over
+function resetGame() {
+  counter = 0;
+  // Resetting best bird score to 0
+  if (bestBird) {
+    bestBird.score = 0;
+  }
+  pipes = [];
+}
+
+// Create the next generation
+function nextGeneration() {
+  resetGame();
+  // Normalize the fitness values 0-1
+  normalizeFitness(allBirds);
+  // Generate a new set of birds
+  activeBirds = generate(allBirds);
+  // Copy those birds to another array
+  allBirds = activeBirds.slice();
+}
+
+// Generate a new population of birds
+function generate(oldBirds) {
+  let newBirds = [];
+  for (let i = 0; i < oldBirds.length; i++) {
+    // Select a bird based on fitness
+    let bird = poolSelection(oldBirds);
+    newBirds[i] = bird;
+  }
+  return newBirds;
+}
+
+// Normalize the fitness of all birds
+function normalizeFitness(birds) {
+  // Make score exponentially better?
+  for (let i = 0; i < birds.length; i++) {
+    birds[i].score = pow(birds[i].score, 2);
+  }
+
+  // Add up all the scores
+  let sum = 0;
+  for (let i = 0; i < birds.length; i++) {
+    sum += birds[i].score;
+  }
+  // Divide by the sum
+  for (let i = 0; i < birds.length; i++) {
+    birds[i].fitness = birds[i].score / sum;
+  }
+}
+
+
+// An algorithm for picking one bird from an array
+// based on fitness
+function poolSelection(birds) {
+  // Start at 0
+  let index = 0;
+
+  // Pick a random number between 0 and 1
+  let r = random(1);
+
+  // Keep subtracting probabilities until you get less than zero
+  // Higher probabilities will be more likely to be fixed since they will
+  // subtract a larger number towards zero
+  while (r > 0) {
+    r -= birds[index].fitness;
+    // And move on to the next
+    index += 1;
+  }
+
+  // Go back one
+  index -= 1;
+
+  // Make sure it's a copy!
+  // (this includes mutation)
+  return birds[index].copy();
+}
---
+++ Source/pipe.js
@@ -0,0 +1,56 @@
+class Pipe {
+  /*setup() {
+    pipeimg = loadImage('pipeimg.png');
+  }*/
+  constructor() {
+
+    // How big is the empty space
+    let spacing = 125;
+    // Where is th center of the empty space
+    let centery = random(spacing, height - spacing);
+
+    // Top and bottom of pipe
+    this.top = centery - spacing / 2;
+    this.bottom = height - (centery + spacing / 2);
+    // Starts at the edge
+    this.x = width;
+    // Width of pipe
+    this.w = 80;
+    // How fast
+    this.speed = 6;
+  }
+
+  // Did this pipe hit a bird?
+  hits(bird) {
+    if ((bird.y - bird.r) < this.top || (bird.y + bird.r) > (height - this.bottom)) {
+      if (bird.x > this.x && bird.x < this.x + this.w) {
+        return true;
+      }
+    }
+    return false;
+  }
+
+  // Draw the pipe
+  show() {
+    stroke(255);
+    fill('#6bdb6b');
+    rect(this.x, 0, this.w, this.top);
+    rect(this.x, height - this.bottom, this.w, this.bottom);
+    //new Image(pipeimg, this.x, 0, this.w, this.top);
+    //new Image(pipeimg, this.x, height - this.bottom, this.w, this.bottom);
+  }
+
+  // Update the pipe
+  update() {
+    this.x -= this.speed;
+  }
+
+  // Has it moved offscreen?
+  offscreen() {
+    if (this.x < -this.w) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+}
